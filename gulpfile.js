987006e042aa0b114d40b3e0dca9ed5d016a2b57
Bastian Stein <stone.bastian@gmail.com>
2016-11-12 19:17:46 +0100

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,44 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 var paths = {
-  es6: 'src/**/*.es6',
-  compiledJs: 'js'
+  es6Src: './src/**/*.es6',
+  mainSrc: './src/main.es6',
+  compiledJsDir: './js/',
+  mainCompile: './js/main.js'
 }
 
 gulp.task('clean', function() {
-  return del([paths.compiledJs]);
+  return del([paths.compiledJsDir]);
 });
 
 gulp.task('build', ['clean'], function() {
-  return gulp.src(paths.es6, {read: false})
-      .pipe(browserify({
-        transform: ['babelify'],
-        extensions: ['.es6']
-      }))
-      // .pipe(babel({ presets: ['es2015'] }))
-        // .on('error', errorHandler)
-      .pipe(gulp.dest(paths.compiledJs));
+  return browserify({
+      extensions: ['.es6'],
+      entries: paths.mainSrc
+  })
+  .transform('babelify')
+  .bundle()
+  .on("error", function (err) { console.log("Error : " + err.message); })
+  .pipe(source('main.js'))
+  .pipe(gulp.dest(paths.compiledJsDir));
+
+  // return gulp.src(paths.mainSrc)
+      // .pipe(browserify({
+      //   extensions: ['.es6']
+      // }))
+      // .bundle()
+      // .pipe(source('main.js'))
+      // .pipe(gulp.dest(paths.compiledJsDir));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(paths.js, ['build']);
+  gulp.watch(paths.es6Src, ['build']);
 });
 
 gulp.task('default', ['watch', 'build'])
