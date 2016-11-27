2e71094fbd9e058e6ddae32dfa3c05f269117ff6
bstein <bstein@thoughtworks.com>
2016-11-27 20:23:57 +0100

    introduced sass/scss

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,46 @@ var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
 
 var paths = {
-  es6Src: './src/**/*.es6',
-  mainSrc: './src/main.es6',
-  compiledJsDir: './js/',
-  mainCompile: './js/main.js'
+    es6Src: './src/**/*.es6',
+    mainSrc: './src/main.es6',
+    compiledJsDir: './js/',
+    scssFiles: './sass/**/*.scss',
+    cssDir: './css/',
+    mainCompile: './js/main.js'
 };
 
 gulp.task('clean', function() {
-  return del([paths.compiledJsDir]);
+    return del([paths.compiledJsDir]);
+});
+
+gulp.task('cleanCss', function() {
+    return del([paths.cssDir]);
 });
 
 gulp.task('build', ['clean'], function() {
-  return browserify({
-      extensions: ['.es6'],
-      entries: paths.mainSrc
-  })
-  .transform('babelify')
-  .bundle()
-  .on("error", function (err) { console.log("Error : " + err.message); })
-  .pipe(source('main.js'))
-  .pipe(gulp.dest(paths.compiledJsDir));
+      return browserify({
+          extensions: ['.es6'],
+          entries: paths.mainSrc
+      })
+      .transform('babelify')
+      .bundle()
+      .on("error", function (err) { console.log("Error : " + err.message); })
+      .pipe(source('main.js'))
+      .pipe(gulp.dest(paths.compiledJsDir));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(paths.es6Src, ['build']);
+    gulp.watch(paths.es6Src, ['build']);
+    gulp.watch(paths.scssFiles, ['sass']);
+});
+
+gulp.task('sass', ['cleanCss'], function () {
+    return gulp.src(paths.scssFiles)
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest(paths.cssDir));
 });
 
 gulp.task('default', ['watch', 'build']);
