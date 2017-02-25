72fe37f79f338f944b92124e536459adcdd090bc
bstein <bstein@thoughtworks.com>
2017-02-25 14:18:20 +0100

    remove unused imports in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
-var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
@@ -13,62 +11,62 @@ if(!isProduction) {
 }
 
 var paths = {
-    es6Src: './src/**/*.es6',
-    mainSrc: './src/main.es6',
-    compiledJsDir: './js/',
-    scssFiles: './sass/**/*.scss',
-    cssDir: './css/',
-    mainCompile: './js/main.js'
+	es6Src: './src/**/*.es6',
+	mainSrc: './src/main.es6',
+	compiledJsDir: './js/',
+	scssFiles: './sass/**/*.scss',
+	cssDir: './css/',
+	mainCompile: './js/main.js'
 };
 
 gulp.task('clean', function() {
-    return del([paths.compiledJsDir]);
+	return del([paths.compiledJsDir]);
 });
 
 gulp.task('cleanCss', function() {
-    return del([paths.cssDir]);
+	return del([paths.cssDir]);
 });
 
 gulp.task('build', ['clean'], function() {
-      return browserify({
-          extensions: ['.es6'],
-          entries: paths.mainSrc
-      })
+	return browserify({
+		extensions: ['.es6'],
+		entries: paths.mainSrc
+	})
       .transform('babelify')
       .bundle()
-      .on("error", function (err) { console.log("Error : " + err.message); })
+      .on('error', function (err) { console.log('Error : ' + err.message); })
       .pipe(source('main.js'))
       .pipe(gulp.dest(paths.compiledJsDir));
 });
 
 gulp.task('watch', function () {
-    gulp.watch(paths.es6Src, ['build']);
-    gulp.watch(paths.scssFiles, ['sass']);
+	gulp.watch(paths.es6Src, ['build']);
+	gulp.watch(paths.scssFiles, ['sass']);
 });
 
 gulp.task('sass', ['cleanCss'], function () {
-    return gulp.src(paths.scssFiles)
+	return gulp.src(paths.scssFiles)
         .pipe(sass.sync().on('error', sass.logError))
         .pipe(gulp.dest(paths.cssDir));
 });
 
 gulp.task('test', function (done) {
-    new Server({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true
-    }, function (exitCode) {
-        done();
-        process.exit(exitCode);
-    }).start();
+	new Server({
+		configFile: __dirname + '/karma.conf.js',
+		singleRun: true
+	}, function (exitCode) {
+		done();
+		process.exit(exitCode);
+	}).start();
 });
 
 gulp.task('tdd', function (done) {
-    new Server({
-        configFile: __dirname + '/karma.conf.js'
-    }, function (exitCode) {
-        done();
-        process.exit(exitCode);
-    }).start();
+	new Server({
+		configFile: __dirname + '/karma.conf.js'
+	}, function (exitCode) {
+		done();
+		process.exit(exitCode);
+	}).start();
 });
 
 gulp.task('default', ['build', 'sass', 'watch']);
