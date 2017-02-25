75e3b78fc50c3ff6a3aa7ff76bc3ee024e69abda
bstein <bstein@thoughtworks.com>
2017-02-25 14:38:11 +0100

    remove unneccessary dependencies from package.json, fix karma.conf for linter

--- karma.conf.js
+++ karma.conf.js
@@ -1,87 +1,74 @@
-// Karma configuration
-// Generated on Sun Nov 13 2016 21:26:37 GMT+0100 (CET)
-
 module.exports = function(config) {
-  config.set({
+	config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
+		basePath: '',
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: [
-      'browserify',
-      'mocha',
-      'chai-sinon',
-      'es6-shim'
-    ],
+		frameworks: [
+			'browserify',
+			'mocha',
+			'chai-sinon',
+			'es6-shim'
+		],
 
     // list of files / patterns to load in the browser
-    files: [
-      'src/**/*.es6',
-      'test/**/*.es6'
-    ],
-
+		files: [
+			'src/**/*.es6',
+			'test/**/*.es6'
+		],
 
     // list of files to exclude
-    exclude: [
-        'src/main.es6'
-    ],
-
+		exclude: [
+			'src/main.es6'
+		],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'src/**/*.es6': ['browserify', 'eslint'],
-      'test/**/*.es6': ['browserify', 'eslint']
-    },
+		preprocessors: {
+			'src/**/*.es6': ['browserify', 'eslint'],
+			'test/**/*.es6': ['browserify', 'eslint']
+		},
 
-    browserify: {
-      debug: true,
+		browserify: {
+			debug: true,
       // transform: ['babelify', {presets: 'es2015'}],
-      configure: function (bundle) {
-        bundle.once('prebundle', function () {
-          bundle.transform('babelify', {presets: 'latest'}).plugin('proxyquireify/plugin');
-        });
-      }
-    },
+			configure: function (bundle) {
+				bundle.once('prebundle', function () {
+					bundle.transform('babelify', {presets: 'latest'}).plugin('proxyquireify/plugin');
+				});
+			}
+		},
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
-
+		reporters: ['mocha'],
 
     // web server port
-    port: 9876,
-
+		port: 9876,
 
     // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
+		colors: true,
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
+		logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
+		autoWatch: true,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
-    // plugins: ['karma-phantomjs-launcher'],
+		browsers: ['PhantomJS'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+		singleRun: false,
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
+		concurrency: Infinity
+	});
 };
